45a8c784f479c682d2369f516618e90d6fa7d01b
Matt Aze <matt.aze10@gmail.com>
2019-07-07 19:40:27 +0100

    basic html outline

---
+++ mycomics.js
@@ -0,0 +1,14 @@
+/**
+ * console-wars-basic.js
+ * version 0.1
+ * sound operations
+ */
+var mycomics = {};
+(function() {
+    let self = this;
+    
+    self.backgroundMusic = function(url) {
+    };
+
+    return self;
+}).apply(mycomics = {});
