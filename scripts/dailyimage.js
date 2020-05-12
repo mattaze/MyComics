e94e658719ec43fa5259513412ceca4d4a48be71
matt Aze <aze.matt10@gmail.com>
2020-05-12 19:31:50 +0100

    added daily image page

---
+++ scripts/dailyimage.js
@@ -0,0 +1,411 @@
+/**
+ * dailyimage.js
+ * version 0.1
+ * full page comic cover of the day (from mycomics library or random of comicvine)
+ * tap page to show details
+ */
+
+let issuedata = {
+	"aliases": null,
+	"api_detail_url": "https://comicvine.gamespot.com/api/issue/4000-65761/",
+	"character_credits": [
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/character/4005-2112/",
+			"id": 2112,
+			"name": "Angel",
+			"site_detail_url": "https://comicvine.gamespot.com/angel/4005-2112/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/character/4005-2157/",
+			"id": 2157,
+			"name": "Cable",
+			"site_detail_url": "https://comicvine.gamespot.com/cable/4005-2157/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/character/4005-7900/",
+			"id": 7900,
+			"name": "Candra",
+			"site_detail_url": "https://comicvine.gamespot.com/candra/4005-7900/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/character/4005-1459/",
+			"id": 1459,
+			"name": "Cyclops",
+			"site_detail_url": "https://comicvine.gamespot.com/cyclops/4005-1459/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/character/4005-24823/",
+			"id": 24823,
+			"name": "Gomurr The Ancient",
+			"site_detail_url": "https://comicvine.gamespot.com/gomurr-the-ancient/4005-24823/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/character/4005-25195/",
+			"id": 25195,
+			"name": "Jamil",
+			"site_detail_url": "https://comicvine.gamespot.com/jamil/4005-25195/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/character/4005-3552/",
+			"id": 3552,
+			"name": "Jean Grey",
+			"site_detail_url": "https://comicvine.gamespot.com/jean-grey/4005-3552/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/character/4005-1445/",
+			"id": 1445,
+			"name": "Juggernaut",
+			"site_detail_url": "https://comicvine.gamespot.com/juggernaut/4005-1445/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/character/4005-15096/",
+			"id": 15096,
+			"name": "Karima",
+			"site_detail_url": "https://comicvine.gamespot.com/karima/4005-15096/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/character/4005-3176/",
+			"id": 3176,
+			"name": "Psylocke",
+			"site_detail_url": "https://comicvine.gamespot.com/psylocke/4005-3176/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/character/4005-2175/",
+			"id": 2175,
+			"name": "Sebastian Shaw",
+			"site_detail_url": "https://comicvine.gamespot.com/sebastian-shaw/4005-2175/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/character/4005-14889/",
+			"id": 14889,
+			"name": "Shadow King",
+			"site_detail_url": "https://comicvine.gamespot.com/shadow-king/4005-14889/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/character/4005-1444/",
+			"id": 1444,
+			"name": "Storm",
+			"site_detail_url": "https://comicvine.gamespot.com/storm/4005-1444/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/character/4005-1440/",
+			"id": 1440,
+			"name": "Wolverine",
+			"site_detail_url": "https://comicvine.gamespot.com/wolverine/4005-1440/"
+		}
+	],
+	"character_died_in": [
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/character/4005-7900/",
+			"id": 7900,
+			"name": "Candra",
+			"site_detail_url": "https://comicvine.gamespot.com/candra/4005-7900/"
+		}
+	],
+	"concept_credits": [],
+	"cover_date": "1997-02-01",
+	"date_added": "2008-06-06 11:16:02",
+	"date_last_updated": "2019-03-19 02:05:21",
+	"deck": null,
+	"description": "<p><em>Warren Worthington arrives at his and Betsy Braddock's apartment to hear a struggle within while Betsy, as Psylocke, battles Undercloaks from the Crimson Dawn!</em></p>",
+	"first_appearance_characters": null,
+	"first_appearance_concepts": null,
+	"first_appearance_locations": null,
+	"first_appearance_objects": null,
+	"first_appearance_storyarcs": null,
+	"first_appearance_teams": null,
+	"has_staff_review": false,
+	"id": 65761,
+	"image": {
+		"icon_url": "https://comicvine1.cbsistatic.com/uploads/square_avatar/5/56044/1811846-x_men__1991__61.jpeg",
+		"medium_url": "https://comicvine1.cbsistatic.com/uploads/scale_medium/5/56044/1811846-x_men__1991__61.jpeg",
+		"screen_url": "https://comicvine1.cbsistatic.com/uploads/screen_medium/5/56044/1811846-x_men__1991__61.jpeg",
+		"screen_large_url": "https://comicvine1.cbsistatic.com/uploads/screen_kubrick/5/56044/1811846-x_men__1991__61.jpeg",
+		"small_url": "https://comicvine1.cbsistatic.com/uploads/scale_small/5/56044/1811846-x_men__1991__61.jpeg",
+		"super_url": "https://comicvine1.cbsistatic.com/uploads/scale_large/5/56044/1811846-x_men__1991__61.jpeg",
+		"thumb_url": "https://comicvine1.cbsistatic.com/uploads/scale_avatar/5/56044/1811846-x_men__1991__61.jpeg",
+		"tiny_url": "https://comicvine1.cbsistatic.com/uploads/square_mini/5/56044/1811846-x_men__1991__61.jpeg",
+		"original_url": "https://comicvine1.cbsistatic.com/uploads/original/5/56044/1811846-x_men__1991__61.jpeg",
+		"image_tags": "All Images"
+	},
+	"issue_number": "61",
+	"location_credits": [
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/location/4020-55799/",
+			"id": 55799,
+			"name": "Hong Kong",
+			"site_detail_url": "https://comicvine.gamespot.com/hong-kong/4020-55799/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/location/4020-57225/",
+			"id": 57225,
+			"name": "Manhattan",
+			"site_detail_url": "https://comicvine.gamespot.com/manhattan/4020-57225/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/location/4020-41183/",
+			"id": 41183,
+			"name": "New York City",
+			"site_detail_url": "https://comicvine.gamespot.com/new-york-city/4020-41183/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/location/4020-56310/",
+			"id": 56310,
+			"name": "New York State",
+			"site_detail_url": "https://comicvine.gamespot.com/new-york-state/4020-56310/"
+		}
+	],
+	"name": "Bolt",
+	"object_credits": [
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/object/4055-41533/",
+			"id": 41533,
+			"name": "Crimson Gem of Cyttorak",
+			"site_detail_url": "https://comicvine.gamespot.com/crimson-gem-of-cyttorak/4055-41533/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/object/4055-56973/",
+			"id": 56973,
+			"name": "Juggernaut's Helmet",
+			"site_detail_url": "https://comicvine.gamespot.com/juggernauts-helmet/4055-56973/"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/object/4055-55652/",
+			"id": 55652,
+			"name": "Ruby Quartz",
+			"site_detail_url": "https://comicvine.gamespot.com/ruby-quartz/4055-55652/"
+		}
+	],
+	"person_credits": [
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/person/4040-41016/",
+			"id": 41016,
+			"name": "Albert Deschesne",
+			"site_detail_url": "https://comicvine.gamespot.com/albert-deschesne/4040-41016/",
+			"role": "letterer"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/person/4040-40985/",
+			"id": 40985,
+			"name": "Bob Harras",
+			"site_detail_url": "https://comicvine.gamespot.com/bob-harras/4040-40985/",
+			"role": "editor"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/person/4040-25695/",
+			"id": 25695,
+			"name": "Cedric Nocon",
+			"site_detail_url": "https://comicvine.gamespot.com/cedric-nocon/4040-25695/",
+			"role": "penciler"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/person/4040-8382/",
+			"id": 8382,
+			"name": "Chad Hunt",
+			"site_detail_url": "https://comicvine.gamespot.com/chad-hunt/4040-8382/",
+			"role": "inker"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/person/4040-5060/",
+			"id": 5060,
+			"name": "Chris Carlson",
+			"site_detail_url": "https://comicvine.gamespot.com/chris-carlson/4040-5060/",
+			"role": "inker, cover"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/person/4040-43762/",
+			"id": 43762,
+			"name": "Jason Liebig",
+			"site_detail_url": "https://comicvine.gamespot.com/jason-liebig/4040-43762/",
+			"role": "editor"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/person/4040-9484/",
+			"id": 9484,
+			"name": "Joe Rosas",
+			"site_detail_url": "https://comicvine.gamespot.com/joe-rosas/4040-9484/",
+			"role": "colorist"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/person/4040-41022/",
+			"id": 41022,
+			"name": "Mark Powers",
+			"site_detail_url": "https://comicvine.gamespot.com/mark-powers/4040-41022/",
+			"role": "editor"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/person/4040-6763/",
+			"id": 6763,
+			"name": "Mike Miller",
+			"site_detail_url": "https://comicvine.gamespot.com/mike-miller/4040-6763/",
+			"role": "inker"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/person/4040-26773/",
+			"id": 26773,
+			"name": "Richard Starkings",
+			"site_detail_url": "https://comicvine.gamespot.com/richard-starkings/4040-26773/",
+			"role": "letterer"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/person/4040-5059/",
+			"id": 5059,
+			"name": "Scott Clark",
+			"site_detail_url": "https://comicvine.gamespot.com/scott-clark/4040-5059/",
+			"role": "penciler, cover"
+		},
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/person/4040-40996/",
+			"id": 40996,
+			"name": "Scott Lobdell",
+			"site_detail_url": "https://comicvine.gamespot.com/scott-lobdell/4040-40996/",
+			"role": "writer"
+		}
+	],
+	"site_detail_url": "https://comicvine.gamespot.com/x-men-61-bolt/4000-65761/",
+	"store_date": null,
+	"story_arc_credits": [],
+	"team_credits": [
+		{
+			"api_detail_url": "https://comicvine.gamespot.com/api/team/4060-3173/",
+			"id": 3173,
+			"name": "X-Men",
+			"site_detail_url": "https://comicvine.gamespot.com/x-men/4060-3173/"
+		}
+	],
+	"team_disbanded_in": [],
+	"volume": {
+		"api_detail_url": "https://comicvine.gamespot.com/api/volume/4050-4605/",
+		"id": 4605,
+		"name": "X-Men",
+		"site_detail_url": "https://comicvine.gamespot.com/x-men/4050-4605/"
+	}
+};
+
+
+var dailyimage = {};
+(function() {
+    let self = this;
+    self.storageKey = "mycomics";
+    
+    //for comic info via mazeediting-comicvine function
+    //`https://mazeediting-comicvine.azurewebsites.net/api/api?code=${AppSettings.MyComics_ComicVineAPI_Key}&comicvineid=${model.comicvineid}`
+    
+    //https://mazeediting-comicvine.azurewebsites.net/api/api?comicvineid=87152
+    //https://mazeediting-comicvine.azurewebsites.net/api/api?code=qifzBr5EaczR2jx9AnWlHgvP4E/U54plU5qBWG1JXz5FW2dg03ZFNg==&comicvineid=4050-87152
+
+
+    //get today image
+
+    //load backup
+
+    //switch off screen
+
+    //timer unhind info page
+    self.ScreenWipeLoopCount = 4;
+    self.ScreenWipe = function() {
+        let overlay = document.getElementsByClassName("screenwiper");
+        if(!overlay) {
+            return;
+        }
+        overlay = overlay[0];
+        overlay.classList.remove("hide");
+        
+        if(self.ScreenWipeLoopCount < 0) {
+            self.ScreenWipeLoopCount = 4;
+            overlay.classList.add("hide");
+        }
+        else {
+            self.ScreenWipeLoopCount--;
+            let r = Math.random() * 255;
+            let g = Math.random() * 255;
+            let b = Math.random() * 255;
+            overlay.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            setTimeout(self.ScreenWipe, 400);
+        }
+    };
+
+    //show offline status
+    self.ResetDisplay = function() {
+        //display none or swipe off the details
+    };
+
+    self.ClockUpdate = function() {
+        let clock = document.getElementById("clock");
+        if(clock) {
+            let now = new Date();
+            now = `${now.getFullYear()}-${lib.pad(now.getMonth()+1, 2)}-${lib.pad(now.getDate(), 2)} ${lib.pad(now.getHours(), 2)}:${lib.pad(now.getMinutes(), 2)}`;
+            clock.textContent = now;
+        }
+    };
+
+    self.GetDailyComicData = function() {
+        //&format=json
+        
+        //http://comicvine.gamespot.com/api/volume/{volume_id}?api_key={api_key}
+        //http://comicvine.gamespot.com/api/issue/{volume_id}?api_key={api_key}
+        //e480a5b3ff1f6b0259db57cf83c3827ec0bafac3
+        //http://comicvine.gamespot.com/api/issues/?api_key=e480a5b3ff1f6b0259db57cf83c3827ec0bafac3&filter=volume:4605
+        //x-men 1991 id = 4050-4605
+
+        //iisue id = 34784
+        //http://comicvine.gamespot.com/api/issues/?api_key=e480a5b3ff1f6b0259db57cf83c3827ec0bafac3&filter=volume:4605
+
+        
+        // call VOLUME - get issues count
+        //https://comicvine.gamespot.com/api/volume/4050-4605?api_key=e480a5b3ff1f6b0259db57cf83c3827ec0bafac3&field_list=count_of_issues&format=json
+            // results.count_of_issues
+        // if over 100 issues - 
+        //https://comicvine.gamespot.com/api/issues/?api_key=e480a5b3ff1f6b0259db57cf83c3827ec0bafac3&field_list=id&format=json&filter=volume:4605&limit=1&offset=33
+            //&offset=  number of set
+            //&limit=1
+            //&offset=55&limit=1
+            //  use offset and limit to just get the one id
+        //get issue info - add issue type to id 4000
+        //https://comicvine.gamespot.com/api/issue/4000-65761/?api_key=e480a5b3ff1f6b0259db57cf83c3827ec0bafac3&format=json
+
+    };
+    self.SaveDailyComicData = function() {
+
+	};
+	
+	self.SetInfo = function() {
+		let infos =  document.querySelectorAll("[data-info]");
+		infos.forEach((elm) => {
+			elm.innerHTML = issuedata[elm.dataset["info"]];
+		});
+	}
+    
+    self.ShowInfo = function() {
+        //on click 
+    }
+
+    self.ClickEvent = function(evt) {
+        document.getElementById("message").innerText = "reloading page";
+        location.reload();
+    };
+
+    self.SetTimers = function(){
+        setInterval(self.ClockUpdate, 3000);
+
+        setInterval(self.ResetDisplay, 60000); //only really needs to run if details clicked to display
+        setInterval(self.ScreenWipe, 600000);
+    };
+
+    self.Load = function(){
+		self.SetInfo();
+        document.addEventListener("click", self.ClickEvent);
+        self.SetTimers();
+    };
+
+
+    return self;
+}).apply(dailyimage = {});
+
+dailyimage.Load();
+
+
+
+
+
+
